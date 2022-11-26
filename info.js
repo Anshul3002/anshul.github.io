274d6c55d793dcb69ac24162cf1aed89fe28a35e
Anshul3002 <118739550+Anshul3002@users.noreply.github.com>
2022-11-26 10:21:12 +0530

    update

--- info.js
+++ info.js
@@ -1,2 +1,2 @@
-const info = [null]
+const info = [{username: 'anshul dhoundiyal', useremail: 'anshuldhoundiyal0@gmail.com', usermessage: 'hello I'm anshul dhoundiyal'},null ]
 module.exports = info;
